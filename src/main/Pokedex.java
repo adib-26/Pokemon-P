f34c02f947455c52186f3ed2d158e3bf99527ee1
wani1009 <152829674+wani1009@users.noreply.github.com>
2024-05-23 22:04:39 +0800

    updated typing map

--- src/main/Pokedex.java
+++ src/main/Pokedex.java
@@ -2,8 +2,11 @@ package main;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 import entity.Pokemon;
 import lib.Moves;
@@ -11,6 +14,10 @@ import lib.Moves;
 public class Pokedex {
     public static Pokemon[] pokedex = new Pokemon[151];
     public static Moves[] allMoves = new Moves[165];
+    public static LinkedHashMap<Integer, String> typeName = new LinkedHashMap<>();
+    public static LinkedHashMap<Integer, ArrayList<Integer>>typeWeakness = new LinkedHashMap<>();
+    public static LinkedHashMap<Integer, ArrayList<Integer>> typeStrength = new LinkedHashMap<>();
+
 
     public static void main(String[] args) throws IOException{
         initializePokedex();
@@ -20,6 +27,8 @@ public class Pokedex {
         Moves.initializeMovesets("src\\res\\movedata.csv", allMoves);
         Pokemon.setUpMoves("src\\res\\pokemonmoveset.csv", pokedex, allMoves);
         Pokemon.setUpEvolution("src\\res\\evoline.csv", pokedex);
+        setUpTypeMapping("src\\res\\typingdata.csv");
+        
 
     }
     
@@ -57,6 +66,35 @@ public class Pokedex {
             }
 }
     return pokedex;
+    }
+
+    public static void setUpTypeMapping(String file){
+        String line = "";
+        BufferedReader reader = null;
+        try{
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            while((line=reader.readLine())!=null){
+                String[] row = line.split(",");
+                if (!(row[1].equals("null"))){
+                  ArrayList<Integer> weakness = new ArrayList<>();
+                  ArrayList<Integer> strength = new ArrayList<>();
+                  String[] weak = row[2].split("@");
+                  String[] strong = row[3].split("@");
+                  for(String w:weak){weakness.add(Integer.parseInt(w));}
+                  for(String s:strong){strength.add(Integer.parseInt(s));}
+                  typeName.put(Integer.parseInt(row[0]), row[1]);
+                  typeWeakness.put(Integer.parseInt(row[0]),weakness);
+                  typeStrength.put(Integer.parseInt(row[0]), strength);
+                  System.out.print("\nAdded new set in map!");
+                } 
+                }
+            
+        } catch(IOException e){e.printStackTrace();}
+        finally{
+            try{reader.close();}
+            catch(IOException e){e.printStackTrace();}
+        }
+
     }
 
   

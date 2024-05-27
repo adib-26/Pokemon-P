60897be265560e028defce951acf505bf07d9e39
Haykee <syakir.asyraaf@gmail.com>
2024-05-27 23:07:51 +0800

    Done Battle Mechanism

--- PokemonBattle/battleMechanism.java
+++ PokemonBattle/battleMechanism.java
@@ -6,16 +6,53 @@ public class battleMechanism {
     private int allyHP,enemyHP,allyAttk,enemyAttk;
     private String allyType,enemyType;
 
+    battleMechanism(){
+        allyHP = 40;
+        enemyHP = 40;
+    }
+
     public static void main(String[] args) {
-        Random r = new Random();
-
+       battleMechanism b = new battleMechanism();
+        System.out.println(b.updateAllyDmg("Grass","Water"));
+        System.out.println(b.updateAllyHP("Grass","Water"));
 
     }
 
-    void checkType(String allyType,String enemyType){
-        if(allyType.equals("Grass") && enemyType.equals("Water")){
+    void setText(){
+        System.out.println("hi");
+    }
+    public int setAllyDmg(){
+       return allyAttk = new Random().nextInt(10);
+    }
 
+    public int updateAllyDmg(String allyType,String enemyType){
+        if(allyType.equals("Grass") && (enemyType.equals("Water")||enemyType.equals("Rock")||enemyType.equals("Ground"))){
+            allyAttk = (setAllyDmg() + setAllyDmg());
         }
+        else if(allyType.equals("Grass") && (enemyType.equals("Fire")||enemyType.equals("Flying")||enemyType.equals("Physics"))){
+            allyAttk = setAllyDmg()/2;
+        }
+        return allyAttk;
     }
 
+    public int updateAllyHP(String allyType,String enemyType){
+       return allyHP -= updateEnemyDmg(allyType,enemyType);
+    }
+
+    public int setEnemyDmg(){
+        return enemyAttk = new Random().nextInt(10);
+    }
+    public int updateEnemyDmg(String allyType,String enemyType){
+        if(allyType.equals("Grass") && (enemyType.equals("Water")||enemyType.equals("Rock")||enemyType.equals("Ground"))){
+            enemyAttk = (setEnemyDmg() + setEnemyDmg());
+        }
+        else if(allyType.equals("Grass") && (enemyType.equals("Fire")||enemyType.equals("Flying")||enemyType.equals("Physics"))){
+            enemyAttk = setAllyDmg()/2;
+        }
+        return enemyAttk;
+    }
+
+    public int updateEnemyHP(String allyType,String enemyType){
+        return enemyHP -= updateAllyDmg(allyType,enemyType);
+    }
 }

69f05381c6e072667d8a3caec723c35e706a90a6
wani1009 <152829674+wani1009@users.noreply.github.com>
2024-05-29 09:10:37 +0800

    implemented sql database

--- src/db/pokemonkantoadventuredb.java
+++ src/db/pokemonkantoadventuredb.java
@@ -1,5 +1,32 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class pokemonkantoadventuredb {
     
+    public static void main(String[] args){
+        createConnection();
+
+    }
+
+    static void createConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pkadb", "root", "root");
+            System.out.println("Connection established!");
+            Statement ex = con.createStatement();
+            ResultSet rs = ex.executeQuery("SELECT * from pokemon");
+            while(rs.next()){
+                String name = rs.getString(2);
+                System.out.println(name);
+            }
+        } catch(SQLException e){Logger.getLogger(pokemonkantoadventuredb.class.getName()).log(Level.SEVERE,null,e);}
+        catch(ClassNotFoundException e){Logger.getLogger(pokemonkantoadventuredb.class.getName()).log(Level.SEVERE,null,e);}
+    }
 }
